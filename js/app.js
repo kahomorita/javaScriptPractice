97ddc477964b5a84837fc785d83386d30c3b698c
kahomorita <kahomaru2679@yahoo.co.jp>
2020-06-09 18:40:15 +0900

    jsで消費税計算

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+{
+
+  const name = window.prompt("名前を入力してください");
+  const total = window.prompt("合計金額を入力して下さい。");
+  const sum = total * 1.10;
+
+  alert(`${name}さんの合計金額は${sum}円となります。`);
+
+
+}
+
