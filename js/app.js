c22ac43d571b81dbf2fb3e4aa8b2562796afa09c
kahomorita <kahomaru2679@yahoo.co.jp>
2020-06-10 17:53:36 +0900

    ９９の計算

--- js/app.js
+++ js/app.js
@@ -1,11 +1,41 @@
 'use strict';
 {
 
-  const name = window.prompt("名前を入力してください");
-  const total = window.prompt("合計金額を入力して下さい。");
-  const sum = total * 1.10;
+  // const name = window.prompt("名前を入力してください");
+  // const total = window.prompt("合計金額を入力して下さい。");
+  // const sum = total * 1.10;
 
-  alert(`${name}さんの合計金額は${sum}円となります。`);
+  // alert(`${name}さんの合計金額は${sum}円となります。`);
+
+
+  // ====メンバーの情報一覧ページ=====
+
+  // const members = [
+
+  //   { name: '山田太郎', age: '42', hobby: 'ギター' },
+  //   { name: '斎藤すぐる', age: '46', hobby: '車' },
+  //   { name: '田中芳雄', age: '32', hobby: '旅行、カメラ' },
+  //   { name: '高橋とおる', age: '25', hobby: '絵を描くこと' },
+
+  // ];
+
+  // const findMembers = members.filter((members) => {
+  //   return members;
+  // });
+  // document.write(findMembers);
+
+
+
+
+  // ＝＝＝＝九九の計算＝＝＝＝
+
+for(let x = 1;x <= 9;x++) {
+  for(let y = 1;y <= 9;y++){
+    document.write(x +'×'+ y + '='+ (x*y)+'<br>');
+  }
+
+}
+
 
 
 }
